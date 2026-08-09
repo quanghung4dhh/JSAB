4e8fe8d83f254b350ab355dac44a9d71d91f292c
dqhung <dinhquanghung446@gmail.com>
2026-08-09 17:05:32 +0700

    *2 done: Draw some rectangles

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const ctx = canvas.getContext("2d");
 
 // console.log(ctx)
 
+//Draw rectangle
+ctx.fillStyle = "blue";
+ctx.fillRect(30, 30, 100, 100);
+ctx.fillStyle = "red";
+ctx.fillRect(950, 590, 100, 100);
+
+
