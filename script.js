a5b385433c3c25b11586668cdbfab8020203521a
dqhung <dinhquanghung446@gmail.com>
2026-08-09 17:12:13 +0700

    Draw rectangle stoke

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ const ctx = canvas.getContext("2d");
 
 // console.log(ctx)
 
-//Draw rectangle
-ctx.fillStyle = "blue";
-ctx.fillRect(30, 30, 100, 100);
-ctx.fillStyle = "red";
-ctx.fillRect(950, 590, 100, 100);
+//Draw rectangle (fill)
+// ctx.fillStyle = "rgba(61, 169, 252, 0.9)";
+// ctx.fillRect(30, 30, 100, 100);
+// ctx.fillStyle = "red";
+// ctx.fillRect(950, 590, 100, 100);
 
+//Draw rectangle (stroke)
+ctx.strokeStyle = "rgba(61, 169, 252, 1)";
+ctx.lineWidth = "2";
+ctx.strokeRect(30, 30, 100, 100);
 
+ctx.strokeStyle = "red";
+ctx.lineWidth = "2";
+ctx.strokeRect(950, 590, 100, 100);
